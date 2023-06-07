bddf781271b2617b6b7c57305cfbfccd821aa017
Rondqd <740242776@qq.com>
2023-06-07 15:26:55 +0800

    修改exe文件

--- chat.py
+++ chat.py
@@ -1,4 +1,4 @@
 import os
 
-os.system("START chrome.exe http://localhost:63342/LenoMate/QA.html?_ijt=ppp0l7keh6tqjdj25vip3030f&_ij_reload=RELOAD_ON_SAVE")
+os.system("START chrome.exe file:///D:/LenoMate/QA.html")
 os.system("cd D:\\LenoMate && D: && activate QA && python web.py")
