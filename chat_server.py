5c0c97ae191a09ce03f574d17d4a223713cac761
Rondqd <740242776@qq.com>
2023-06-29 14:57:31 +0800

    前后端分离：后端v2

--- chat_server.py
+++ chat_server.py
@@ -9,6 +9,9 @@ import torch
 import audio.play as play
 import audio.synthesis as synthesis
 
+import operation.prompt as prompt
+import operation.operation_server as operation
+
 def sys(result):
     synthesis.main(result)
     play.play()
@@ -16,9 +19,12 @@ def sys(result):
 def load_and_run_model():
     global input_queue,output_queue
     print("模型加载中...")
-    model = AutoModel.from_pretrained("THUDM/chatglm-6b-int4", trust_remote_code=True).half().cuda()
+    model = AutoModel.from_pretrained(r"C:\Users\89721\Desktop\models--THUDM--chatglm2-6b-int4", trust_remote_code=True).cuda()
     # model = AutoModel.from_pretrained(r"C:\Users\Opti7080\Desktop\models--THUDM--chatglm-6b", trust_remote_code=True).half().cuda()
-    tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b-int4", trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(r"C:\Users\89721\Desktop\models--THUDM--chatglm2-6b-int4", trust_remote_code=True)
+    # model = AutoModel.from_pretrained("THUDM/chatglm-6b-int4", trust_remote_code=True).half().cuda()
+    # model = AutoModel.from_pretrained(r"C:\Users\Opti7080\Desktop\models--THUDM--chatglm-6b", trust_remote_code=True).half().cuda()
+    # tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b-int4", trust_remote_code=True)
     model_sim =  AutoModel.from_pretrained("GanymedeNil/text2vec-large-chinese").to('cuda')
     tokenizer_sim = AutoTokenizer.from_pretrained("GanymedeNil/text2vec-large-chinese")
     corpus = faiss_corpus(model = model_sim, tokenizer=tokenizer_sim)
@@ -26,30 +32,37 @@ def load_and_run_model():
 
     while True: 
         data_client = input_queue.get() # load data from client mode = 0是命令模式
+        input_statement = data_client['inputs']
         if data_client['state_code'] == 0:
             if not data_client['mode']:
                 print("当前为命令模式...")
-                input_statement = data_client['inputs']
+                
                 try:
                     selected_idx, score = corpus.search(query=input_statement, verbose=True)
                     if selected_idx in [0,1,4,5]:
-                        output_queue.put
-                    torch.cuda.empty_cache()
-                    if selected_idx == 5:
-                        opt = eval(f"operation.Operation{selected_idx}")(input_statement,model_sim,tokenizer_sim)
+                        command = f"python operation/operation_client.py --select-idx {selected_idx}"
+                        client_data = {}
+                        client_data['command']=command
+                        client_data['state_code']=1
+                        output_queue.put(str(client_data))
+                        
                     else:
-                        opt = eval(f"operation.Operation{selected_idx}")(input_statement)
-                    result = opt.fit(model, tokenizer)
-                    output_queue.put((result, True))
-                    print("模型输出：", result)
+                        torch.cuda.empty_cache()
+                        if selected_idx == 5:
+                            opt = eval(f"operation.Operation{selected_idx}")(input_statement,model_sim,tokenizer_sim)
+                        else:
+                            opt = eval(f"operation.Operation{selected_idx}")(input_statement)
+                        result = opt.fit(model, tokenizer)
+                        output_queue.put((result, True))
+                        print("模型输出：", result)
                 except Exception as e:
                     print('#' * 50)
                     print('error info', e)
-                    break
-            if data_client['mode']:
+                    continue
+            elif data_client['mode']:
                 print("当前为聊天模式...")
                 prompt_chat = f"""<用户>：{input_statement}
-                <ChatGLM-6B>："""
+                <LenoMate>:"""
                 model.eval()
                 with torch.no_grad():
                     input_ids = tokenizer.encode(prompt_chat, return_tensors='pt').to('cuda')
@@ -59,8 +72,21 @@ def load_and_run_model():
                         temperature=0.9,
                         top_p=0.95,
                     )
-                answer = tokenizer.decode(out[0]).split('<ChatGLM-6B>:')[1].strip('\n').strip()
-                output_queue.put((answer, True))
+                answer = tokenizer.decode(out[0]).split('<LenoMate>:')[1].strip('\n').strip()
+                send_data ={'chat':answer}
+                output_queue.put(str(send_data))
+        if data_client['state_code'] == 1:
+            context = data_client['inputs']
+            torch.cuda.empty_cache()
+            if selected_idx == 5:
+                opt = eval(f"operation.Operation{selected_idx}")(input_statement,model_sim,tokenizer_sim)
+            else:
+                opt = eval(f"operation.Operation{selected_idx}")(input_statement,context)
+            result = opt.fit(model, tokenizer)
+
+            output_queue.put(result)
+            print("模型输出：", result)
+            
 
 
 
@@ -69,7 +95,7 @@ def handle_client(client_socket, client_address):
     input_queue_client = queue.Queue()
     output_queue_client = queue.Queue()
     print(f"已与{client_address[0]}:{client_address[1]}建立连接")
-    temp_data = "{'command':'START chrome.exe http://localhost:8081/'}"
+    temp_data = "{'chat':'你好，我是LenoMate，请问有什么可以帮您','command':'START chrome.exe http://localhost:8081/'}"
     client_socket.send(temp_data.encode('utf-8'))
     while True:
         try:
@@ -78,15 +104,12 @@ def handle_client(client_socket, client_address):
             if data:
                 print(f"收到{client_address[0]}:{client_address[1]}的消息：{data}")
                 # 广播消息给所有连接的客户端
-                
-                input_queue_client.put(eval(data))  
-                
+                input_queue_client.put(eval(data))    
                 data_temp = input_queue_client.get()
                 input_queue.put(data_temp) # put data into input_queue
                 # input_queue.put(input_queue_client.get()
-
                 send_data = output_queue.get()
-                output_queue_client.put()
+                output_queue_client.put(send_data)
                 client_socket.send(output_queue_client.get().encode('utf-8'))
                 print(f"回复{client_address[0]}:{client_address[1]}的消息：{data}")
         except Exception as e:
@@ -96,8 +119,17 @@ def handle_client(client_socket, client_address):
             break
 
 
+# 创建一个输入队列
+input_queue,output_queue = queue.Queue(),queue.Queue()
+# 创建一个线程，用于加载和运行模型
+model_thread = threading.Thread(target=load_and_run_model)
+model_thread.daemon = True
+# 启动线程
+model_thread.start()
 
-def main(ip='192.168.137.1'):
+
+
+def chat_server(ip='192.168.137.1'):
 # 创建套接字
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # 监听端口
@@ -109,4 +141,7 @@ def main(ip='192.168.137.1'):
         client_socket, client_address = server_socket.accept()
         # 创建线程处理客户端连接
         client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-        client_thread.start()
+        client_thread.start()    
+
+if __name__ == '__main__':
+    chat_server()
---
+++ operation/operation_server.py
@@ -0,0 +1,257 @@
+import pynvml
+import screen_brightness_control as sbc
+import time
+import os
+import argparse
+import torch
+import operation.prompt as prompt
+import re
+from operation.open_app import search_tool
+import numpy as np
+
+class Operation:
+    def fit(self):
+        raise NotImplementedError
+
+## 放入context给出数值
+class Operation0(Operation):
+    def __init__(self,input_statement,context) -> None:
+        self.context = context
+        self.inputs = input_statement
+
+    def fit(self,model,tokenizer):
+        self.prompt = prompt.Prompt0(self.inputs,self.context).prompt
+        self.extract_info(self.prompt,model,tokenizer)
+        self.brightness = self.num
+        res = {
+            'chat':self.answer,
+            'state_code':0,
+            'command':f"python operation\screen_brightness.py --bright {self.num}"
+        }
+        return res
+            
+    def extract_info(self,prompt,model,tokenizer):
+        model.eval()
+        with torch.no_grad():
+            input_ids = tokenizer.encode(prompt, return_tensors='pt').to('cuda')
+            out = model.generate(
+                input_ids=input_ids,
+                max_length=200,
+                temperature=0.3,
+                top_p = 0.95,
+                # repetition_penalty = 1.15,
+                # stopping_criteria = StoppingCriteriaList([stop_criteria])
+                # do_sample = True
+            )
+            self.answer = tokenizer.decode(out[0]).split('<bot>:')[1]
+            print('[屏幕亮度调节功能]',self.answer)
+            self.num = int(re.findall(r"\d+\.?\d*",self.answer)[-1])
+
+
+class Operation1(Operation):
+    def __init__(self,**inputs) -> None:
+        pass
+    def fit(self):
+        pynvml.nvmlInit()
+        handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+        info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+        
+        context = f'''总显存: {info.total/1024**2:.2f} MB
+空闲显存: {info.free/1024**2:.2f} MB
+已使用显存: {info.used/1024**2:.2f} MB'''
+        return context
+
+
+default_path = os.path.join(os.path.expanduser("~"), "Desktop") ## desktop_path
+def time_suffix():
+    return time.strftime("%Y%m%d%H%M%S", time.localtime(time.time()))
+
+class Operation2(Operation):
+    def __init__(self,inputs,path=default_path,summary='notebook'):
+        self.path = path
+        self.inputs = time.strftime("%Y%m%d", time.localtime(time.time()))+':'+inputs
+        self.summary = summary
+        
+    def fit(self,model,tokenizer):
+        self.prompt = prompt.Prompt2(self.inputs).prompt
+        model.eval()
+        with torch.no_grad():
+            input_ids = tokenizer.encode(self.prompt, return_tensors='pt').to('cuda')
+            out = model.generate(
+                input_ids=input_ids,
+                max_length=200,
+                temperature=0.3,
+                top_p = 0.95,
+                # repetition_penalty = 1.15,
+                # stopping_criteria = StoppingCriteriaList([stop_criteria])
+                # do_sample = True
+            )
+            print('operation3 output',tokenizer.decode(out[0]).split('##总结:'))
+            answer = tokenizer.decode(out[0]).split('##总结:')[1].strip()
+            
+            self.summary = answer.strip('。')
+
+        file_name = 'LenoMate_'+self.summary+'_'+time_suffix()+'.txt'
+        file_name = os.path.join(default_path,file_name)
+        with open(file_name,'w',encoding='utf-8') as f:
+            f.write(self.inputs)
+        f.close()
+        return f'已完成记录，保存在桌面，文件名称为{file_name}'
+    
+# def get_parser():
+#     parser = argparse.ArgumentParser('Opening APP')
+    
+#     parser.add_argument('--new-embed', default=False,type=bool)
+#     parser.add_argument('--document-embed', default='./data/document_embed.npy')
+#     parser.add_argument('--k', default=1)
+#     parser.add_argument('--device', default="cuda")
+#     parser.add_argument('--model-name', default="GanymedeNil/text2vec-large-chinese")
+
+#     parser.add_argument('--index_location', default='./data/app_map.index')
+#     parser.add_argument('--document-corpus', default='./data/app.txt')
+#     parser.add_argument('--search', default=2)
+#     args = parser.parse_args()      
+#     return args
+# args_app = get_parser()  
+
+class Operation3(Operation):
+    def __init__(self,inputs):
+        self.tool = search_tool()
+        self.input_statement = inputs
+        
+        
+    def fit(self,model=None,tokenizer=None):
+        from utils.search_doc_faiss import faiss_corpus
+        from data.map import name_exe_map
+        args_app = self.get_parser() 
+        corpus = faiss_corpus(args = args_app)
+        selected_idx,score = corpus.search(query = self.input_statement,verbose=True)
+        app_name = corpus.corpus[selected_idx]
+        print(f'find app {app_name}')
+        return self.tool.open_app(b = name_exe_map[app_name])
+    
+    def get_parser(self):
+        parser = argparse.ArgumentParser('Opening APP')
+        
+        parser.add_argument('--new-embed', default=False,type=bool)
+        parser.add_argument('--document-embed', default='./data/document_embed.npy')
+        parser.add_argument('--k', default=1)
+        parser.add_argument('--device', default="cuda")
+        parser.add_argument('--model-name', default="GanymedeNil/text2vec-large-chinese")
+
+        parser.add_argument('--index_location', default='./data/app_map.index')
+        parser.add_argument('--document-corpus', default='./data/app.txt')
+        parser.add_argument('--search', default=2)
+        args = parser.parse_args()      
+        return args
+     
+    
+class Operation4(Operation):
+    def __init__(self,inputs): 
+        from operation.get_cominfo import computer_info
+        import pythoncom
+        computer = computer_info()
+        pythoncom.CoInitialize()
+        self.context = computer.fit()
+        self.inputs = inputs
+
+    def fit(self,model,tokenizer):
+        self.prompt = prompt.Prompt1(self.inputs,self.context).prompt
+        model.eval()
+        with torch.no_grad():
+            input_ids = tokenizer.encode(self.prompt, return_tensors='pt').to('cuda')
+            out = model.generate(
+                input_ids=input_ids,
+                max_length=400,
+                temperature=0.3,
+                top_p = 0.95,
+                # repetition_penalty = 1.15,
+                # stopping_criteria = StoppingCriteriaList([stop_criteria])
+                # do_sample = True
+            )
+            answer = tokenizer.decode(out[0]).split('##回答')[1].strip(':').strip()
+
+            return answer
+        
+class Operation5():
+    def __init__(self,inputs,model_sim,tokenizer_sim) -> None:
+        from operation.volumn_control import vol_ctrl
+        self.vol_ctrl = vol_ctrl()
+        self.inputs = inputs
+        self.model_sim = model_sim
+        self.tokenizer_sim = tokenizer_sim
+
+    def fit(self,model,tokenizer,remote = False):
+        self.judge()
+        if self.selected=='静音': 
+            self.vol_ctrl.mute_all()
+            return "已静音"
+        if self.selected=='取消静音': 
+            self.vol_ctrl.mute_all(mute=False)
+            return "已取消静音"
+        if not remote:
+            self.vl = self.vol_ctrl.vl_real
+            context = '电脑当前音量为'+str(self.vl)
+        if remote:
+            context = remote
+        self.prompt = prompt.Prompt4(context,self.inputs).prompt
+        self.extract_info(self.prompt,model,tokenizer)
+        self.vol = self.num
+        try:
+            self.vol_ctrl.alter(self.num)
+            return self.answer
+        except:
+            return f'对不起，我没识别到您的指令，请重新描述~'
+
+    def judge(self):
+        _mute = '静音'
+        _normal = '调节音量'
+        _no_mute = '取消静音'
+        corpus = [_mute,_normal,_no_mute]
+        self.model_sim.eval()
+        input_data = self.tokenizer_sim(self.inputs, return_tensors="pt")
+        corp = self.tokenizer_sim(corpus, return_tensors="pt",padding=True)
+        with torch.no_grad():
+            corpus_embeddings = self.model_sim(**corp.to('cuda'))
+            input_embeddings = self.model_sim(**input_data.to('cuda'))
+        corpus_embeddings = self._pooling(corpus_embeddings,attention_mask=corp['attention_mask'])
+        input_embeddings = self._pooling(input_embeddings,attention_mask=input_data['attention_mask'])
+        self.selected = corpus[(corpus_embeddings@input_embeddings.T).argmax(axis=0)[0]]
+        print(f"[音量调节功能]音量功能匹配为'{self.selected}'")
+
+    def _pooling(self,model_output,attention_mask):
+        token_embeddings = model_output[0]  # First element of model_output contains all token embeddings
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        output = torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+        return self._l2_normalization(output.cpu().numpy())
+    
+    def _l2_normalization(self,data):
+        if data.ndim == 1:
+            return data / np.linalg.norm(data).reshape(-1,1)
+        else:
+            return data/ np.linalg.norm(data,axis=1).reshape(-1,1)
+        
+    def extract_info(self,prompt,model,tokenizer):
+        model.eval()
+        with torch.no_grad():
+            input_ids = tokenizer.encode(prompt, return_tensors='pt').to('cuda')
+            out = model.generate(
+                input_ids=input_ids,
+                max_length=200,
+                temperature=0.3,
+                top_p = 0.95,
+                # repetition_penalty = 1.15,
+                # stopping_criteria = StoppingCriteriaList([stop_criteria])
+                # do_sample = True
+            )
+            self.answer = tokenizer.decode(out[0]).split('<bot>:')[1]
+            print('[音量调节功能]',self.answer)
+            self.num = int(re.findall(r"\d+\.?\d*",self.answer)[-1])
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser('Client Operation')
+    parser.add_argument('--select-idx', default=0)
+    args = parser.parse_args() 
+    operation = eval(f"Operation{args.select_idx}")()
+    print(operation.fit())
