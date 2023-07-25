243e1b6108913941dc8f83135aa7c2d7a2143259
Southpika <513923576@qq.com>
2023-07-25 12:48:36 +0800

    README修改

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # LenoMate
-LenoMate 
 
-# 指令更新操作
+LenoMate是一款智能桌面助手，可以帮助用户操作电脑或者进行聊天类功能
+
+底层大语言模型是一款基于chatglm2进行微调，
+
+聊天类功能为日常聊天，网页搜索问答，PPT总结以及讲稿，PDF总结
+
+操作类功能目前支持屏幕亮度调节，音量调节，备忘录，打开软件，电脑配置查询
+
+![1690260335920](image/README/1690260335920.png)
+
+![1690260367506](image/README/1690260367506.png)
+
+# 操作指令更新操作
+
 每次加入新的指令(data/document_corpus)，需更新操作库的embedding, 命令行运行 python utils/renew_corpus.py
 
 # windows的bitsandbytes使用如下仓库
-https://github.com/jllllll/bitsandbytes-windows-webui
 
+https://github.com/jllllll/bitsandbytes-windows-webui
--- utils/web_search.py
+++ utils/web_search.py
@@ -3,6 +3,7 @@ from urllib.parse import quote
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+
 def search_web(keyword):
 	options = webdriver.ChromeOptions()
 	options.add_argument('headless')
@@ -32,6 +33,17 @@ def search_web(keyword):
 				title = item_prelist["titletext"]
 				relist.append([title, url])
 	return relist
+
+def search_wx(url):
+	headers = {
+		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.44",
+	}
+	r = requests.get(url, headers=headers)
+	html = r.text
+	soup = BeautifulSoup(html, 'html.parser')
+	item_list = soup.find(class_='rich_media_wrp')
+	item_title = re.sub(r'(<[^>]+>|\s)','',str(item_list))
+	return item_title
 
 def search_zhihu_que(url):
     headers = {
@@ -122,16 +134,19 @@ class web_searcher:
                 return_content.append(str(search_zhihu_que(ext_zhihu(items[1])))[0:500]) #int(get_config().get("web").get('web_max_length'))
                 reference_list.append(items[1])
             if "baike.sogou.com" in items[1] or '百科' in feature:
-                return_content.append(str(search_baike_sougou(items[1]))[0:500])
+                return_content.append(str(search_baike_sougou(items[1]))[0:1000])
                 reference_list.append(items[1])
             if "zhidao.baidu.com" in items[1] or '百度知道' in feature:
                 return_content.append(str(search_baidu_zhidao(items[1]))[0:200])
                 reference_list.append(items[1])
             if "zhuanlan.zhihu.com" in items[1] or '知乎专栏' in feature:
-                return_content.append(str(search_zhihu_zhuanlan(items[1]))[0:500])
+                return_content.append(str(search_zhihu_zhuanlan(items[1]))[0:1000])
                 reference_list.append(items[1])
             if 'new.qq.com' in items[1]:
-                return_content.append(str(search_tencent_news(items[1]))[0:500])
+                return_content.append(str(search_tencent_news(items[1]))[0:1000])
+                reference_list.append(items[1])
+            if "mp.weixin.qq.com" in items[1] and '微信公众号' in feature:
+                return_content.append(str(search_wx(items[1]))[0:1000])
                 reference_list.append(items[1])
         return [reference_list[:self.web_num],return_content[:self.web_num]]
 
