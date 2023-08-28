30fc2ad40c36155baccb60c2976f185f21f8c98f
“Southpika” <513923576@qq.com>
2023-08-28 16:42:38 +0800

    客户端init

--- operation/__init__.py
+++ operation/__init__.py
@@ -1,18 +1,20 @@
-# from .create_notebook import *
-# from .display_graphic import *
+from .create_notebook import *
+from .display_graphic import *
 from .fortune import *
-# from .get_cominfo import *
-# from .hello import *
-# from .open_app import *
+from .get_cominfo import *
+from .hello import *
+from .open_app import *
 # from .operation import *
-# from .operation_client import *
-from .operation_server import *
+from .operation_client import *
+# from .operation_server import *
 from .prompt import *
-# from .read_file import *
-# from .screen_brightness import *
+from .volumn_control import *
+from .screen_brightness import *
+
 # from .search import *
+# from .read_file import *
 # from .test import *
 # from .test_bright import *
-# from .volumn_control import *
+
 
 
