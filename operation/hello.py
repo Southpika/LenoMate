384e4de71ce769e9b78a220d3030bbb8b9d98bf4
Tzu-cheng Chang <zzz128@stu.pku.edu.cn>
2023-11-21 14:21:22 +0800

    Beijing client v1

--- operation/hello.py
+++ operation/hello.py
@@ -4,7 +4,7 @@ import operation.fortune as fortune
 class bot_hello:
     def __init__(self, system='Windows', language='ch') -> None:
         self.language = language
-        self.query_data = '你好，我是LenoMate。Building Bridges, one byte at a time.' if language == 'cn' else 'Hello, I am LenoMate. Building Bridges, one byte at a time.'
+        self.query_data = '你好，我是LenoMate。Building Bridges, one byte at a time. ' if language == 'cn' else 'Hello, I am LenoMate. Building Bridges, one byte at a time. '
         self.system = system
 
     def hello(self, ast=True):
@@ -14,7 +14,7 @@ class bot_hello:
             ast_query = ast.constellation()
             if ast_query:
                 self.query_data += '\n您的今日运势为:\n' + ast_query
-        self.query_data += '请问有什么可以帮您?' if self.language == 'cn' else 'What can I do for you? '
+        self.query_data += '请问有什么可以帮您?' if self.language == 'cn' else 'What can I do for you?'
         hello = {'chat': self.query_data,
                  'end': True,
                  'command': "os.system('START msedge.exe http://localhost:8081/')" 
